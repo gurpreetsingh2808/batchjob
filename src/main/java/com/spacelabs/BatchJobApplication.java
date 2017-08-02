f7a137dbbee938cbf4bd7348a284641729002ce1
Gurpreet Singhgit config --global user.email gurpreetsingh2808@gmail.com <gurpreetsingh2808@gmail.com>
2017-08-02 13:25:23 +0530

    Initial commit

---
+++ src/main/java/com/spacelabs/BatchJobApplication.java
@@ -0,0 +1,16 @@
+package com.spacelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+public class BatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchJobApplication.class, args);
+	}
+}
