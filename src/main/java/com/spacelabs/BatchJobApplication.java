22a8b677c9a4867263cce47f19468abed0bc0669
Gurpreet Singhgit config --global user.email gurpreetsingh2808@gmail.com <gurpreetsingh2808@gmail.com>
2017-08-02 14:16:50 +0530

    Added and completed person job
    Added sample-data.csv for job
    Added schema for person table
    Updated pom.xml file

--- src/main/java/com/spacelabs/BatchJobApplication.java
+++ src/main/java/com/spacelabs/BatchJobApplication.java
@@ -1,16 +1,12 @@
 package com.spacelabs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@Configuration
 public class BatchJobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchJobApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchJobApplication.class, args);
+    }
 }
---
+++ src/main/java/com/spacelabs/jobs/personjob/dto/Person.java
@@ -0,0 +1,13 @@
+package com.spacelabs.jobs.personjob.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String lastName;
+    private String firstName;
+}
